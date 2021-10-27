138a4898d56055f4c9746c570e8bef3b917ceb5e
Saaya Yamanaka <ayakmp0623@gmail.com>
2021-10-27 15:26:24 +0900

    Angularフロントエンド側作成

---
+++ src/app/products.ts
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    heding01: 'sample text 1',
+    heding02: 'sample text 2',
+    heding03: 'sample text 3',
+    heding01text: 'sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text',
+    heding02text: 'sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text',
+    heding03text: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    heding01: 'sample text 1',
+    heding02: 'sample text 2',
+    heding03: 'sample text 3',
+    heding01text: 'sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text',
+    heding02text: 'sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text',
+    heding03text: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone Standard',
+    price: 299,
+    description: 'A great phone with one of the best cameras',
+    heding01: 'sample text 1',
+    heding02: 'sample text 2',
+    heding03: 'sample text 3',
+    heding01text: 'sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text',
+    heding02text: 'sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text',
+    heding03text: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Pone Standard',
+    price:999,
+    description: 'A great phone with one of the best cameras',
+    heding01: 'sample text 1',
+    heding02: 'sample text 2',
+    heding03: 'sample text 3',
+    heding01text: 'sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text',
+    heding02text: 'sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text',
+    heding03text: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+  }
+];
