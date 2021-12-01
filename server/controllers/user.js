e0034b3631e58a571c46ddfa30d0207a9d5d59e8
Saaya Yamanaka <ayakmp0623@gmail.com>
2021-12-01 10:58:02 +0900

    AuthMiddlewareとAuthGuard追加

---
+++ server/controllers/user.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken');
+const User = require('../model/user');
+const config = require('../config');
+
+function notAuthorized(res) {
+    return res.status(401).send({
+        errors: [{ title: 'Not Authorized error', detail: 'You need to login!' }]
+    })
+}
+
+exports.authMiddleware = function(req, res, next) {
+
+    const token = req.headers.authorization
+
+    if (!token) {
+        return notAuthorized(res)
+    }
+
+    jwt.verify(token.split(' ')[1], config.SECRET, function(err, decodedToken) {
+        if (err) {
+            return res.status(401).send({
+                errors: [{ title: 'Not Authorized error', detail: 'Invalid token!' }]
+            })
+        }
+        User.findById(decodedToken.userId, function(err, foundUser) {
+            if (err) {
+                return res.status(401).send({
+                    errors: [{ title: 'Not Authorized error', detail: 'Invalid token!' }]
+                })
+            }
+
+            if (!foundUser) {
+                return res.status(401).send({
+                    errors: [{ title: 'Not Authorized error', detail: 'Invalid token!' }]
+                })
+            }
+            next()
+        })
+    })
+}
